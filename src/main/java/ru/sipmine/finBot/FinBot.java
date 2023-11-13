9b88683527284a8fcc14312ad148fd35b138451a
sipmine <gleb.tedpoope@gmail.com>
2023-11-13 09:28:05 +0700

    sync

--- src/main/java/ru/sipmine/finBot/FinBot.java
+++ src/main/java/ru/sipmine/finBot/FinBot.java
@@ -1,9 +1,13 @@
 package ru.sipmine.finBot;
 
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,55 +15,70 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import com.google.protobuf.Extension.MessageType;
 
-public class FinBot extends TelegramLongPollingBot{
+import ru.sipmine.finBot.BotCommands.StartCommand;
+
+
+public class FinBot extends TelegramLongPollingCommandBot{
     private String botName;
     private String botToken;
 
     public FinBot(String botName, String botToken) {
         this.botName = botName;
         this.botToken = botToken;
+        register(new StartCommand());
 
+        
     }
     
     @Override
     public String getBotUsername() {
-        // TODO
         return botName;
     }
 
     @Override
     public String getBotToken() {
-        // TODO
+        
         return botToken;
     }
+
+
+
     @Override
-    public void onUpdateReceived(Update update) {
-       
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String message = update.getMessage().getText();
-            System.out.println(message);
-            if (message.equals("/mark")) {
-            System.out.println(1);
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.setChatId(update.getMessage().getChatId().toString());
-            sendMessage.setText("your kb");
-            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-            List<KeyboardRow> keyboardRows = new ArrayList<>();
-
-            KeyboardRow row = new KeyboardRow();
- 
-            keyboardRows.add(row);
-            keyboardMarkup.setKeyboard(keyboardRows);
-            sendMessage.setReplyMarkup(keyboardMarkup);
-            try {
-                execute(sendMessage);    
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-            
-            }
-
-        }
+    public void processNonCommandUpdate(Update update) {
+        
     }
+
+
+
+    // @Override
+    // public void onUpdateReceived(Update update) {
+       
+    //     if (update.hasMessage() && update.getMessage().hasText()) {
+    //         String message = update.getMessage().getText();
+    //         System.out.println(message);
+    //         if (message.equals("/mark")) {
+    //         System.out.println(1);
+    //         SendMessage sendMessage = new SendMessage();
+    //         sendMessage.setChatId(update.getMessage().getChatId().toString());
+    //         sendMessage.setText("your kb");
+    //         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+    //         List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+    //         KeyboardRow row = new KeyboardRow();
+ 
+    //         keyboardRows.add(row);
+    //         keyboardMarkup.setKeyboard(keyboardRows);
+    //         sendMessage.setReplyMarkup(keyboardMarkup);
+    //         try {
+    //             execute(sendMessage);    
+    //         } catch (TelegramApiException e) {
+    //             e.printStackTrace();
+    //         }
+            
+    //         }
+
+    //     }
+    // }
 } 
