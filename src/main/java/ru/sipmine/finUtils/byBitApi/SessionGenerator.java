f2fa0b6d92795a7402eec70a2b824893c9be2aae
sipmine <gleb.tedpoope@gmail.com>
2023-12-17 18:38:19 +0700

    Bybit create session

--- src/main/java/ru/sipmine/finUtils/byBitApi/SessionGenerator.java
+++ src/main/java/ru/sipmine/finUtils/byBitApi/SessionGenerator.java
@@ -3,6 +3,18 @@ package ru.sipmine.finUtils.byBitApi;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.google.gson.Gson;
 
 import jakarta.websocket.ClientEndpoint;
 
@@ -10,6 +22,8 @@ import jakarta.websocket.ClientEndpoint;
 public class SessionGenerator {
     private String apiKeySecret;
     private String apiKey;
+    private String timestamp = Long.toString(ZonedDateTime.now().toInstant().toEpochMilli());
+    private final String RECV_WINDOW = "5000";
 
     public SessionGenerator(String apiKeySecret, String apiKey) {
         this.apiKeySecret = apiKeySecret;
@@ -17,7 +31,7 @@ public class SessionGenerator {
 
     }
 
-    public  int getConnect() {
+    public int checkConnect() {
         int statusCode = -1;
         try {
             URL url = new URL("https://api.bybit.com");
@@ -30,8 +44,82 @@ public class SessionGenerator {
         }
         return statusCode;
     }
-    
-    // create header for connection [] []
 
+    public HttpURLConnection ConnectionGET(String endpoint, Map map)
+            throws InvalidKeyException, NoSuchAlgorithmException {
+        HttpURLConnection connection = null;
+        String sign = genGetSign(map);
+        StringBuilder sb = genQueryStr(map);
+        try {
+            URL url = new URL("https://api.bybit.com" + endpoint + "?" + sb);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("X-BAPI-API-KEY", apiKey);
+            connection.setRequestProperty("X-BAPI-SIGN", sign);
+            connection.setRequestProperty("X-BAPI-TIMESTAMP", timestamp);
+            connection.setRequestProperty("X-BAPI-RECV-WINDOW", RECV_WINDOW);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
+    private String genPostSign(Map<String, Object> params) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(apiKeySecret.getBytes(), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+
+        String paramJson = new Gson().toJson(params);
+        String sb = timestamp + apiKey + RECV_WINDOW + paramJson;
+        return bytesToHex(sha256_HMAC.doFinal(sb.getBytes()));
+    }
+
+    /**
+     * The way to generate the sign for GET requests
+     * 
+     * @param params: Map input parameters
+     * @return signature used to be a parameter in the header
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+    private String genGetSign(Map<String, Object> params) throws NoSuchAlgorithmException, InvalidKeyException {
+        StringBuilder sb = genQueryStr(params);
+        String queryStr = timestamp + apiKey + RECV_WINDOW + sb;
+
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(apiKeySecret.getBytes(), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        return bytesToHex(sha256_HMAC.doFinal(queryStr.getBytes()));
+    }
+
+    /**
+     * To convert bytes to hex
+     * 
+     * @param hash
+     * @return hex string
+     */
+    private String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1)
+                hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    private StringBuilder genQueryStr(Map<String, Object> map) {
+        Set<String> keySet = map.keySet();
+        Iterator<String> iter = keySet.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            sb.append(key)
+                    .append("=")
+                    .append(map.get(key))
+                    .append("&");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb;
+    }
 }
--- src/test/java/ByBitConnectTest.java
+++ src/test/java/ByBitConnectTest.java
@@ -1,20 +1,65 @@
 import static org.junit.Assert.assertEquals;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.mockito.Mockito;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
-import jakarta.websocket.EndpointConfig;
-import jakarta.websocket.Session;
+import org.junit.Test;
+
 import ru.sipmine.finUtils.byBitApi.SessionGenerator;
 
 public class ByBitConnectTest {
     @Test
-    public void getConnectTest() {
+    public void checkConnectToServerTest() {
         SessionGenerator sessionGenerator = new SessionGenerator(null, null);
-        assertEquals(sessionGenerator.getConnect(), 200);
+        assertEquals(sessionGenerator.checkConnect(), 200);
 
     }
-    
-   
+
+    @Test
+    public void checkConnectionToEndpointTest() throws InvalidKeyException, NoSuchAlgorithmException {
+        SessionGenerator sessionGenerator = new SessionGenerator("",
+                "");
+        Map<String, Object> map = new HashMap<>();
+        map.put("accountType", "UNIFIED");
+        HttpURLConnection connection = sessionGenerator.ConnectionGET("/v5/account/wallet-balance", map);
+        try {
+            assertEquals(connection.getResponseCode(), 200);
+
+            // Получение InputStream для чтения ответа от сервера
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            // Закрытие потоков чтения
+            reader.close();
+            inputStream.close();
+
+            // Ваш JSON-ответ
+            String jsonResponse = response.toString();
+            System.out.println(jsonResponse); // Вывод JSON-ответа в консоль
+
+            // Запись ответа в файл
+            FileWriter writer = new FileWriter("response.json");
+            writer.write(jsonResponse);
+            writer.close();
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
